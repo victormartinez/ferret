71462147425c9fb60ca0ed4be1e396a77c17ec66
Victor Martinez <martinezvcr@gmail.com>
2016-07-06 00:02:12 -0300

    Refactor requirements to add most recent scrapely library
    - Create requirements-scrapely.txt
    - Add scrapely dependencies to requirements
    - Refactor setup file

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ def _to_list(requires):
     return [str(ir.req) for ir in requires]
 
 install_requires = _to_list(parse_requirements('requirements.txt', session=False))
+scrapely_requires = _to_list(parse_requirements('requirements-scrapely.txt', session=False))
 tests_require = _to_list(parse_requirements('requirements-dev.txt', session=False))
 
 
 setup(
     name='ferret',
     version='1.0',
-    install_requires=install_requires,
+    install_requires=install_requires + scrapely_requires,
     tests_require=tests_require,
     setup_requires=['pytest-runner==2.7.1'],
     packages=find_packages(),
