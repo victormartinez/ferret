9ea0bb48b77dde4e4edc27a4c76bf409484532b4
Victor Martinez <martinezvcr@gmail.com>
2016-07-07 01:01:18 -0300

    Improve html region extraction and add todo
    - Analyse the ceil in the function to improve the heuristic

--- ferret/util/html_region.py
+++ ferret/util/html_region.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from math import ceil
 from ferret.cleaner.cleaner import clean_body
-from scrapely.htmlpage import HtmlPage, HtmlTag
+from scrapely.htmlpage import HtmlPage
 
 
 def extract_text_region_around_title(html):
     html_text = clean_body(html)
     html_page = HtmlPage(body=html_text)
-    html_tags = [r for r in html_page.parsed_body if isinstance(r, HtmlTag)]
-    index = ceil(len(html_tags) / 3)
-    start = html_tags[index].start
-    end = html_tags[index * 2].end
+    index = ceil(len(html_page.parsed_body) / 3)
+    start = html_page.parsed_body[index].start
+    end = html_page.parsed_body[(index * 2)].end  # TODO: Analyse the necessity of + ceil(index/2)
     return html_page.body[start:end]
