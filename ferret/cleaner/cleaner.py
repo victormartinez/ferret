50870a3fbc83464f92668ed14ee30809a4bd6d74
Victor Martinez <martinezvcr@gmail.com>
2016-07-07 01:02:52 -0300

    Refactor basic html cleaner

--- ferret/cleaner/cleaner.py
+++ ferret/cleaner/cleaner.py
@@ -2,13 +2,13 @@
 from bs4 import BeautifulSoup, Comment
 
 TAGS_TO_REMOVE = ['script', 'style', 'iframe', 'meta', 'link', 'form', 'embed', 'noscript', 'object', 'video', 'source',
-                  'svg', 'use', 'code', 'pre', 'img']
+                  'svg', 'use', 'code', 'pre', 'img', 'input']
 
 
 def clean_body(html):
     soup = BeautifulSoup(html, 'html.parser')
-    body_doc = soup.body
-    for tag in body_doc.find_all(True):
+    soup.head.extract()
+    for tag in soup.find_all(True):
         if tag.name in TAGS_TO_REMOVE:
             tag.extract()
 
@@ -23,11 +23,11 @@ def clean_body(html):
         if tag.is_empty_element:
             tag.extract()
 
-    comments = body_doc.find_all(text=lambda text: isinstance(text, Comment))
+    comments = soup.find_all(text=lambda text: isinstance(text, Comment))
     for comment in comments:
         comment.extract()
 
-    result = str(body_doc).replace('\n', '')
+    result = str(soup).replace('\n', '')
     result = result.replace('\t', '')
     result = result.replace('\r', '')
     result = result.replace('  ', ' ')
