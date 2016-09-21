84cd2ff5641516620efcaeefa28cefabe1b91fa3
Victor Martinez <vcrmartinez@gmail.com>
2016-09-20 23:33:15 -0300

    Add tests for tag title extractor

---
+++ test/extractors/title/tag_title_extractor.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pytest
+from ferret.extractors.title_extractor import TagTitleExtractor
+
+
+def _get_og_title_extractor(html):
+    return TagTitleExtractor(html)
+
+
+def _get_contents_of_file(path):
+    with open(path, 'r') as content_file:
+        return content_file.read()
+
+@pytest.mark.parametrize("test_file_path,expected_title", [
+    ("pt/portal-justificando-a-reforma-trabalhisa-propoe-uma-nova-forma-de-escravidao.html", "A Reforma Trabalhisa propõe uma nova forma de escravidão"),
+    ("pt/tj-mg-unidades-do-cejusc-sao-inauguradas-em-sabara-e-em-coronel-fabriciano.html", "Unidades do Cejusc são inauguradas em Sabará e em Coronel Fabriciano"),
+    ("pt/tj-sc-empresa-aerea-indeniza-casal-que-pagou-assento-conforto-mas-viajou-apertado-ate-miami.html", "Empresa aérea indeniza casal que pagou assento conforto mas viajou apertado até Miami"),
+    ("pt/tre-pa-justica-eleitoral-do-para-inicia-geracao-de-midia-para-urnas-eletronicas.html", "Justiça Eleitoral do Pará inicia geração de mídia para urnas eletrônicas"),
+    ("pt/tre-pr-tre-pr-decide-sobre-o-tempo-participacao-de-apoiadores-no-horario-eleitoral.html", "TRE-PR decide sobre o tempo participação de apoiadores no horário eleitoral"),
+])
+def test_tag_title_extractor(test_file_path, expected_title):
+    html = _get_contents_of_file("../../resources/{}".format(test_file_path))
+    extractor = _get_og_title_extractor(html)
+    assert extractor.extract() == expected_title
