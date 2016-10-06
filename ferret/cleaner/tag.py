700ea9d1f159c962894e974341c12ddcf477fe70
Victor Martinez <vcrmartinez@gmail.com>
2016-10-06 00:31:37 -0300

    Refactor tag module

--- ferret/cleaner/tag.py
+++ ferret/cleaner/tag.py
@@ -8,15 +8,16 @@ TAGS_TO_REMOVE = ['script', 'style', 'iframe', 'meta', 'link', 'form', 'noscript
 
 TAGS_TO_UNWRAP = ['font']
 
-UNWANTED_ATTRS_REGEX = "sidebar|widget|social|facebook|comment|tweet|menu|footer|subscribe|foot|nav|google|share|search" \
-                       "|form|contact|breadcrumb|banner|advertisement|lang|btn|tab|sitemap|instagram|flickr|print" \
-                       "|button|pinterest|radio|bread|icon|dusqus|sponsor|popup|modal|pagination|promo" \
-                       "|related|scroll|tool|login|sign|next|prev|old|new|shop|continue|fb-|messenger"
+UNWANTED_ATTRS_REGEX = "sidebar|widget|social|facebook|comment|tweet|menu|footer|subscribe|foot|nav|google|share|search"\
+                       "|form|contact|breadcrumb|banner|advertisement|lang|btn|tab|sitemap|instagram|flickr|print"\
+                       "|button|pinterest|radio|bread|icon|dusqus|sponsor|popup|modal|pagination"\
+                       "|related|scroll|tool|login|sign|next|prev|shop|continue|fb-|messenger|header|meta"
 
 
-def remove_id_and_class(tag):
-    for attr in ["class", "id", "name", "style", "align"]:
-        if attr in tag.attrs.keys():
+def remove_unnecessary_attributes(tag):
+    keys = list(tag.attrs.keys())
+    for attr in keys:
+        if attr not in ['src', 'href']:
             del tag.attrs[attr]
 
 
