5b64a6e7d1e82604b9f2070eaf41b1e29e004cc3
Victor Martinez <vcrmartinez@gmail.com>
2016-11-03 01:08:37 -0300

    Major refactor in main module

--- ferret/main.py
+++ ferret/main.py
@@ -1,47 +1,82 @@
-import re
-from ferret.analyser import TitleAnalyser, PublishedDateAnalyser
+from ferret.cleaner.cleaner import extract_body_text_from_html
+from ferret.cleaner.text import normalize_text
+from ferret.extractors.published_date_extractor import OpenGraphPublishedDateExtractor, UrlPublishedDateExtractor, \
+    TimeTagExtractor, PatternPublishedDateExtractor, MetaTagsPublishedDateExtractor, PublishedDateNearTitleExtractor
+from ferret.extractors.title_extractor import OpenGraphTitleExtractor, UrlTitleExtractor, TitleCandidateExtractor, \
+    TitleTagExtractor
 from ferret.util.http import get_html
+from langdetect import detect
+from toolz import dicttoolz
 
 
 class Ferret:
-    def __init__(self, url, html=None):
+    def __init__(self, url, html=None, lang=None):
         if html is not None and not isinstance(html, str):
             raise ValueError("HTML must be a string.")
-        self.url = url
-        self.raw_html = self._get_html(url, html)
-        self.title_analyser = TitleAnalyser(url, self.raw_html)
-        self.date_analyser = PublishedDateAnalyser(url, self.raw_html)
+
+        self.basic_context = {'html': self._get_html(url, html), 'url': url, 'lang': self._get_lang(lang, html)}
+        self.title_extractors = (
+            OpenGraphTitleExtractor,
+            UrlTitleExtractor,
+            TitleCandidateExtractor,
+            TitleTagExtractor
+        )
+
+        self.date_extractors = (
+            OpenGraphPublishedDateExtractor,
+            UrlPublishedDateExtractor,
+            TimeTagExtractor,
+            PublishedDateNearTitleExtractor,
+            PatternPublishedDateExtractor,
+            MetaTagsPublishedDateExtractor
+        )
 
     def _get_html(self, url, html):
-        if html is None:
-            self.raw_html = get_html(url)
-        return html
+        return html if html is not None else get_html(url)
+
+    def _get_lang(self, lang, html):
+        if not lang:
+            cleaned_text = extract_body_text_from_html(html)
+            return detect(cleaned_text)
+        return lang
 
     def get_article(self):
-        return {
-            'title': self.extract_title(),
-            'published_date': self.extract_published_date(),
-            'html_content': self.extract_html_content()
-        }
+        title = self.extract_title(self.basic_context)
+        context_with_title = dicttoolz.merge(self.basic_context, {'title': title})
 
-    def extract_title(self):
-        extractors = self.title_analyser.next_extractor()
+        published_date = self.extract_published_date(context_with_title)
+        context_with_date = dicttoolz.merge(context_with_title, {'published_date': published_date})
+
+        content = self.extract_html_content(context_with_date)
+        return dicttoolz.merge(context_with_date, {'content': content})
+
+    def extract_title(self, context=None):
+        if context is None:
+            context = self.basic_context
+
+        extractors = self._get_extractors(self.title_extractors, context)
         for e in extractors:
             title = e.extract()
             if title:
-                return title
+                return normalize_text(title)
         return ''
 
-    def extract_published_date(self):
-        extractors = self.date_analyser.next_extractor()
+    def extract_published_date(self, context=None):
+        if context is None:
+            context = self.basic_context
+
+        extractors = self._get_extractors(self.date_extractors, context)
         for e in extractors:
             datetime = e.extract()
-            if self._is_date_valid(datetime):
-                return str(datetime)
+            if datetime is not None:
+                return datetime
+        return None
+
+    def extract_html_content(self, context):
         return ''
 
-    def _is_date_valid(self, date):
-        return re.search(r'(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})/?', str(date))
-
-    def extract_html_content(self):
-        return ''
+    def _get_extractors(self, extractors, context):
+        for extractor_instance in extractors:
+            extractor = extractor_instance(context)
+            if extractor.is_suitable():
+                yield extractor
