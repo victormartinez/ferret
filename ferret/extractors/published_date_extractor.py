f2a2e2ee5fc5d55e6ad248cd91ca0c5c38352d90
Victor Martinez <vcrmartinez@gmail.com>
2016-11-03 23:32:49 -0300

    Improve date extraction in order to use language

--- ferret/extractors/published_date_extractor.py
+++ ferret/extractors/published_date_extractor.py
@@ -1,25 +1,27 @@
 import dateparser
 import re
 from bs4 import BeautifulSoup
-from dateutil import parser
 from ferret.cleaner.cleaner import extract_body_text_from_html
 from ferret.cleaner.text import normalize_text
-from ferret.util.date import parse_into_friendly_string, DATE_PATTERNS
+from ferret.util.date import find_and_parse_into_date, parse_to_date
 from math import ceil
 
 
 class UrlPublishedDateExtractor:
     def __init__(self, context):
-        self.url = context.get('url')
+        self.context = context
         self.regex = r'(\d{4}/\d{2}/\d{2})/'
 
     def is_suitable(self):
-        published_date = re.match(self.regex, self.url)
+        published_date = re.match(self.regex, self.context.get('url'))
         return published_date is not None
 
     def extract(self):
-        published_date = re.search(self.regex, self.url)
-        datetime = parser.parse(published_date.group())
+        published_date = re.search(self.regex, self.context.get('url'))
+        datetime = parse_to_date(published_date.group(), self.context.get('lang'))
+        if not datetime:
+            return None
+
         if datetime.tzinfo:
             return datetime.isoformat()[:-6]
         return datetime.isoformat()
@@ -27,21 +29,28 @@ class UrlPublishedDateExtractor:
 
 class MetaTagsPublishedDateExtractor:
     def __init__(self, context):
-        self.raw_html = context.get('html')
+        self.context = context
 
     def is_suitable(self):
-        soup = BeautifulSoup(self.raw_html, 'html.parser')
+        soup = BeautifulSoup(self.context.get('html'), 'html.parser')
         meta_tags = soup.select('meta')
         return meta_tags
 
     def extract(self):
-        soup = BeautifulSoup(self.raw_html, 'html.parser')
+        soup = BeautifulSoup(self.context.get('html'), 'html.parser')
         meta_tags = soup.select('meta')
         for tag in meta_tags:
             content = tag.attrs.get('content')
+            if not content:
+                continue
+
             match = re.match(r'\d{4}-\d{2}-\d{2}T?\d{2}:\d{2}:\d{2}', content)
             if content and match:
-                datetime = parser.parse(match.group())
+                # It's not necessary to use the lang once the meta tag brings the timezone
+                datetime = dateparser.parse(match.group())
+                if not datetime:
+                    return None
+
                 if datetime.tzinfo:
                     return datetime.isoformat()[:-6]
                 return datetime.isoformat()
@@ -50,36 +59,47 @@ class MetaTagsPublishedDateExtractor:
 
 class TimeTagExtractor:
     def __init__(self, context):
-        self.raw_html = context.get('html')
+        self.context = context
 
     def is_suitable(self):
         time_tag = self._get_time_tag()
         return time_tag and time_tag.get('datetime')
 
     def _get_time_tag(self):
-        soup = BeautifulSoup(self.raw_html, 'lxml')
+        soup = BeautifulSoup(self.context.get('html'), 'lxml')
         return soup.select_one('time')
 
     def extract(self):
-        return self._get_time_tag().get('datetime')
+        date_str = self._get_time_tag().get('datetime')
+        datetime = parse_to_date(date_str, self.context.get('lang'))
+        if not datetime:
+            return None
+
+        if datetime.tzinfo:
+            return datetime.isoformat()[:-6]
+        return datetime.isoformat()
 
 
 class OpenGraphPublishedDateExtractor:
     def __init__(self, context):
-        self.raw_html = context.get('html')
+        self.context = context
 
     def is_suitable(self):
-        soup = BeautifulSoup(self.raw_html, 'lxml')
+        soup = BeautifulSoup(self.context.get('html'), 'lxml')
         date_tag = soup.select_one('meta[property=article:published_time]')
         return date_tag and date_tag.get('content')
 
     def extract(self):
-        soup = BeautifulSoup(self.raw_html, 'lxml')
+        soup = BeautifulSoup(self.context.get('html'), 'lxml')
         published_time = soup.select_one('meta[property=article:published_time]')
         if not published_time:
             return None
 
-        datetime = parser.parse(published_time.get('content'))
+        # It's not necessary to use the lang once the meta tag brings the timezone
+        datetime = dateparser.parse(published_time.get('content'))
+        if not datetime:
+            return None
+
         if datetime.tzinfo:
             return datetime.isoformat()[:-6]
         return datetime.isoformat()
@@ -100,10 +120,10 @@ class PublishedDateNearTitleExtractor:
         start_index = self._get_start_index(text, 500)
         end_index = self._get_end_index(text, 500)
         date = self.extract_date(text, start_index, end_index, offset)
-        if not date:
+        if date is None:
             return None
 
-        datetime = dateparser.parse(date, languages=[self.context.get('lang')])
+        datetime = parse_to_date(date, self.context.get('lang'))
         if not datetime:
             return None
 
@@ -124,8 +144,8 @@ class PublishedDateNearTitleExtractor:
 
     def extract_date(self, text, start_index, end_index, offset):
         fragment = text[start_index:end_index]
-        candidate = self._extract_date_candidate(fragment)
-        if candidate:
+        candidate = find_and_parse_into_date(fragment)
+        if candidate is not None:
             return candidate
 
         if len(fragment) >= len(text):
@@ -141,39 +161,22 @@ class PublishedDateNearTitleExtractor:
             return 0
         return next_start_index
 
-    def _extract_date_candidate(self, fragment):
-        text = normalize_text(fragment)
-        for date_pattern in DATE_PATTERNS:
-            search = re.search(date_pattern, text, re.I)
-            if search:
-                candidate = parse_into_friendly_string(search.group())
-                if self._is_candidade_valid(candidate):
-                    return candidate
-
-        return None
-
-    def _is_candidade_valid(self, candidate):
-        return candidate.count('/') == 2 or candidate.count('.') == 2 or candidate.count('-') == 2 or candidate.count(
-            '/') == 2
-
 
 class PatternPublishedDateExtractor:
     def __init__(self, context):
-        self.url = context.get('url')
-        self.raw_html = context.get('html')
-        self.lang = context.get('lang')
+        self.context = context
 
     def is_suitable(self):
         return True
 
     def extract(self):
-        text = normalize_text(normalize_text(extract_body_text_from_html(self.raw_html)))
+        text = normalize_text(normalize_text(extract_body_text_from_html(self.context.get('html'))))
         start_index = ceil(len(text) / 3)
         date = self.extract_date(text, start_index, start_index)
         if not date:
             return None
 
-        datetime = dateparser.parse(date, languages=[self.lang])
+        datetime = parse_to_date(date, self.context.get('lang'))
         if not datetime:
             return None
 
@@ -186,7 +189,7 @@ class PatternPublishedDateExtractor:
         offset_down = 100
 
         fragment = text[start_index:end_index]
-        candidate = self._extract_date_candidate(fragment)
+        candidate = find_and_parse_into_date(fragment)
         if candidate:
             return candidate
 
@@ -196,20 +199,6 @@ class PatternPublishedDateExtractor:
         next_start_index = self._get_next_start_index(start_index, offset_up)
         next_end_index = end_index + offset_down
         return self.extract_date(text, next_start_index, next_end_index)
-
-    def _extract_date_candidate(self, text):
-        for date_pattern in DATE_PATTERNS:
-            search = re.search(date_pattern, text, re.I)
-            if search:
-                candidate = parse_into_friendly_string(search.group())
-                if self._is_candidade_valid(candidate):
-                    return candidate
-
-        return None
-
-    def _is_candidade_valid(self, candidate):
-        return candidate.count('/') == 2 or candidate.count('.') == 2 or candidate.count('-') == 2 or candidate.count(
-            '/') == 2
 
     def _get_next_start_index(self, start_index, offset):
         next_start_index = start_index - offset
--- ferret/util/date.py
+++ ferret/util/date.py
@@ -1,11 +1,65 @@
 # -*- coding: utf-8 -*-
+import dateparser
 import re
+
+
+def replace_str_by_number(str_date):
+    for month, number in DATE_REPLACEMENTS:
+        if re.search(month, str_date, re.I):
+            return re.sub(month, number, str_date)
+    return str_date
+
+
+def date_replace_1(date_str):
+    return date_str.replace('de', '/')
+
+
+def date_replace_2(date_str):
+    return date_str.replace('às', '')
+
+
+def date_replace_3(date_str):
+    return date_str.replace('|', '')
+
+
+def date_replace_4(date_str):
+    return date_str.replace('h', ':')
+
+
+def date_replace_5(date_str):
+    return date_str.replace(' /', '/')
+
+
+def date_replace_6(date_str):
+    return date_str.replace(' ', '')
+
+
+def date_replace_7(date_str):
+    return date_str.replace('//', '/')
+
+
+def date_replace_8(date_str):
+    return date_str.replace('min', '')
+
+
+def date_replace_9(date_str):
+    return date_str.replace('-/', '-')
+
+
+def date_replace_10(date_str):
+    return date_str.replace('/-', '-')
+
 
 DATE_PATTERNS = [
 
-    r'\d{4}/\d{2}/\d{2}',  # 2016/04/01
-    r'\w+\s\d,\s\d{4}',  # May 1, 2016
-    r'\d{2}\s\w{3}\s\d{4}',  # 01 May 2016
+    # 2016/04/0
+    (r'\d{4}/\d{2}/\d{2}', []),
+
+    # May 1, 201: []
+    (r'\w+\s\d,\s\d{4}', []),
+
+    # 01 May 201: []
+    (r'\d{2}\s\w{3}\s\d{4}', []),
 
     # 02/02/2016 às 14h10
     # 02/02/2016 às 14h10min
@@ -52,7 +106,7 @@ DATE_PATTERNS = [
     #
     # 5/7/2016 às 21h18
     # And the same above for 02.02.02
-    r'\d{1,4}.\d{1,2}.\d{2,4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?',
+    (r'\d{1,4}.\d{1,2}.\d{2,4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?', [date_replace_4, date_replace_8]),
 
     # 26 de abril de 2012
     # 09 de fevereiro de 2015 às 12:40
@@ -63,7 +117,8 @@ DATE_PATTERNS = [
     # 13 de março de 2013 - 19:35:00
     # 13 de março de 2013 - 19h35min
     # 5 de julho de 2016 - 11h25
-    r'(?<!\d)\d{1,2}\sde\s[\wç]+\sde\s\d{4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?',
+    (r'(?<!\d)\d{1,2}\sde\s[\wç]+\sde\s\d{4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?',
+     [replace_str_by_number, date_replace_1, date_replace_7, date_replace_2, date_replace_4, date_replace_8]),
 
     # Terça, (26 Junho 2012)
     # Terça, (26 Junho 2012 16:02)
@@ -72,42 +127,75 @@ DATE_PATTERNS = [
     # Terça, (26 Junho 2012 08:36)
     # Segunda, (25 Junho 2012 15:27)
     # quinta-feira, (22 de julho de 2009)
-    r'\d{2}\s(de\s)?[\wç]+\s(de\s)?\d{4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?',
-
-    # 2015 - junho - 29
-    r'\d{4}\s-\s[\wç]+\s-\s\d{2}',
+    (r'\d{2}\s(de\s)?[\wç]+\s(de\s)?\d{4}(\s?(às|-)?\s(\d{2}[:h]\d{2}(min)?(:\d{2})?))?', []),
 
     # 2016-10-25 19:57:02
-    r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}',
+    (r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', []),
 
-    r'\d{2}-\w+-\d{4}',
+    (r'\d{2}-\w+-\d{4}', []),
 
-    r'\d{2}/\d{2}/\d{4}',
+    (r'\d{2}/\d{2}/\d{4}', []),
 
     # 2016 - outubro - 25
-    r'\d{4}\s?-\s?[\wç]+\s?-\s?\d{2}'
+    (r'\d{4}\s?-\s?[\wç]+\s?-\s?\d{2}', [replace_str_by_number, date_replace_6, date_replace_9, date_replace_10]),
+
+    # 24 Out 2016 19:02
+    (r'\d{2}\s[\wç]{3}\s\d{4}\s\d{2}:\d{2}', [replace_str_by_number, date_replace_5]),
 ]
 
 DATE_REPLACEMENTS = [
-    (r'janeiro|january', '01'),
-    (r'fevereiro|february', '02'),
-    (r'mar[çc]o|march', '03'),
-    (r'abril|april', '04'),
-    (r'maio|may', '05'),
-    (r'junho|june', '06'),
-    (r'julho|july', '07'),
-    (r'agosto|august', '08'),
-    (r'setembro|september', '09'),
-    (r'outubro|october', '10'),
-    (r'novembro|november', '11'),
-    (r'dezembro|december', '12'),
+    (r'janeiro|january|jan', '/01/'),
+    (r'fevereiro|february|fev|feb', '/02/'),
+    (r'mar[çc]o|march|mar', '/03/'),
+    (r'abril|april|abr|apr', '/04/'),
+    (r'maio|may|mai', '/05/'),
+    (r'junho|june|jun', '/06/'),
+    (r'julho|july|jul', '/07/'),
+    (r'agosto|august|aug', '/08/'),
+    (r'setembro|september|sep', '/09/'),
+    (r'outubro|october|out|oct', '/10/'),
+    (r'novembro|november|nov', '/11/'),
+    (r'dezembro|december|dez|dec', '/12/'),
 ]
 
 
-def parse_into_friendly_string(str_date):
-    new_date = str_date.lower()
-    for month, number in DATE_REPLACEMENTS:
-        if re.search(month, str_date, re.I):
-            new_date = re.sub(month, number, new_date)
+def find_and_parse_into_date(text_fragment):
+    fragment = text_fragment.lower()
+    candidates = []
+    for t in DATE_PATTERNS:
+        regex, replacement_strategies = t
+        match = re.search(regex, fragment, re.I)
+        if match:
+            candidate = match.group()
+            if _is_candidade_valid(candidate):
+                date_str = apply_strategies(candidate, replacement_strategies)
+                candidates.append(date_str)
 
-    return new_date.replace('de', '').replace('às', '').replace('|', '').replace('h', ':').strip()
+    if len(candidates):
+        return candidates[0]
+    return None
+
+
+def apply_strategies(str_date, replacement_functions):
+    new_str_date = str_date
+    for function in replacement_functions:
+        new_str_date = function(new_str_date)
+    return new_str_date
+
+
+def _is_candidade_valid(candidate):
+    return candidate.count('/') == 2 or candidate.count('.') == 2 or candidate.count('-') == 2 or candidate.count(
+        '/') == 2 or candidate.count('de') == 2 or candidate.count(':') == 2 or _string_contains_month(candidate)
+
+
+def _string_contains_month(candidate):
+    regex = r'janeiro|january|jan|fevereiro|february|fev|feb|mar[çc]o|march|mar|abril|april|abr|apr|maio|may|mai|junho|june|jun|julho|july|jul|agosto|august|aug|setembro|september|sep|outubro|october|out|oct|novembro|november|nov|dezembro|december|dez|dec'
+    return re.search(regex, candidate, re.I)
+
+
+def parse_to_date(date_str, lang):
+    datetime = dateparser.parse(date_str, languages=[lang])
+    if datetime:
+        return datetime
+
+    return dateparser.parse(date_str)
