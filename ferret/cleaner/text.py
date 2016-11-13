75143b5c59a9a459cd5fb28e7b7b1c6f47dbb4a3
Victor Martinez <vcrmartinez@gmail.com>
2016-11-13 16:33:24 -0300

    Improve special chars removal

--- ferret/cleaner/text.py
+++ ferret/cleaner/text.py
@@ -1,10 +1,14 @@
 def remove_special_chars(body_str):
     if body_str is None:
         return ''
+
     result = body_str.replace('\n', '')
+    result = result.replace('\\n', '')
     result = result.replace('\t', '')
-    result = result.replace('\b', '')
-    return result.replace('\r', '')
+    result = result.replace('\\t', '')
+    result = result.replace('\r', '')
+    result = result.replace('\\r', '')
+    return result
 
 
 def normalize_text(text):
